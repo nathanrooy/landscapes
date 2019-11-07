c7616a84f20a43bdf4709a8ba4628b2e52aa6573
Nathan Rooy <nathanrooy@gmail.com>
2019-11-07 17:04:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='landscapes',
     version='0.0.1',
     author='Nathan A. Rooy',
     author_email='nathanrooy@gmail.com',
     url='https://github.com/nathanrooy/landscapes',
+    description='A Python library of standardized optimization test functions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=['landscapes'],
     python_requires='>=3.5',
     classifiers=[
