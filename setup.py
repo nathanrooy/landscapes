e52d4fc30bd6bb318d01e42f7bf521552d947778
Nathan Rooy <nathanrooy@gmail.com>
2020-05-17 14:03:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='landscapes',
-    version='0.0.6',
+    version='0.0.7',
     author='Nathan A. Rooy',
     author_email='nathanrooy@gmail.com',
     url='https://github.com/nathanrooy/landscapes',
